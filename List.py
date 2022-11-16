5630014b948d61d7a22778823d4d298a1b44160a
Tahmauri01 <tbobo@brooklynsteamcenter.org>
2022-11-16 09:27:18 -0500

    Made lists

---
+++ List.py
@@ -0,0 +1,15 @@
+myList = ['burger', 'lettuce', 'cheese', 'bread', 'water']
+print (len(myList))
+print (myList)
+
+myList.reverse()
+print(myList)
+
+yourList = ['brown', 'red', 'black', 'purple', 'blue']
+print(yourList)
+
+print(myList + yourList)
+
+print("???".join(myList + yourList))
+
+print('no,yes,maybe'.split(','))    
